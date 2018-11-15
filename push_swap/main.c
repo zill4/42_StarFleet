da919de1e60f40b8598defc535902a400ca1d060
Justin Crisp <jcrisp@e1z4r15p4.42.us.org>
2018-11-14 18:48:34 -0800

    More pointer functions

--- push_swap/main.c
+++ push_swap/main.c
@@ -13,46 +13,75 @@
 #include "./include/swap.h"
 
 
-void sa()
+//Swap the first 2 elemetns at the top of stack a. Do nothing if tehre is only one or no elements.
+void sa(t_list *head)
 {
+	void *temp;
+	void *temp_2;
+	t_list *l_temp;
 
+
+	temp = head->content; 
+	temp_2 = head->next->content;
+	head->content = temp_2;
+	head->next->content = temp;
 }
-void sb()
+//Take the first elemetn at the top of b and put it at hte top of a. Do nothin if b is empty.
+void pa(t_list *origin, t_list *dest)
 {
-
+	void *temp;
+	void *temp_2;
+	t_list *temple;
+	//Must delete the original content and move the list forward.
+	//Must push the next one to the other list. 
+	temp = origin->content;
+	//PUSHED TO THE NEXT LIST.
+	pushNum(*(int*)temp, &dest);
+	//I want to die but I can't I have to sit here listening to voice in my head telling me the things that I did wrong.
+	//The chit chat in my head is stuck on a loop, and I am looking for an escape. I keep looking for someting, but I don't want to find myself.
+	free(origin->content);
+	origin->content_size = 0;
+	temple = origin->next;
+	free(origin);
+	origin = temple;
 }
-void ss()
+
+//shift up all elements of stack a by 1.The first element becomes the last one.
+void ra(t_list *head)
 {
+	//Rotate this list down one and take the last put it on top.
+	t_list *temp;
+	void *temple;
+	size_t semple;
 
+	temp = head;
+	while (temp->next);
+		temp = temp->next;
+	temple = temp->content;
+	semple = temp->content_size;
+	free(temp->content);
+	free(temp);
+	temp = ft_lstnew(temple, sizeof(int));
+	temp->next = head;
+	head = temp;
 }
-void pa()
-{
 
-}
-void pb()
-{
-
-}
-void ra()
-{
-
-}
-void rb()
-{
-
-}
+//ra and rb at the same time.
 void rr()
 {
 
 }
+//Shift down all elements of stack a by 1. The last element becomes the first one.
 void rra()
 {
 
 }
+//Shift down all elements of stack b by 1. The last element becomes the first one.
 void rrb()
 {
 
 }
+//rra and rrb at the same time. 
 void rrr()
 {
 
@@ -148,27 +177,51 @@ void	readArgs(int argc, char** args, t_list **list)
 	}
 	
 }
+/* void		checkFunk(char *flags, t_list *head_a, t_list *head_b)
+ * 
+ *
+ */
 
-void	checkFunk(char *flags)
+void	checkFunk(char *flags, t_list *head_a, t_list *head_b)
 {
 	//Functions Sa, Sb, Ss, pa, pb, ra, rb, rr, rra, rrb, rrr
-	static void (*pf[])(void) = {sa, sb, ss, pa, pb, ra, rb, rr, rra, rrb, rrr };
+	static void (*pf[])(void) = {sa, pa, ra, rb, rr, rra, rrb, rrr };
 	if (ft_strcmp("sa",flags) == 0)
-		pf[0];
+	{
+		//Swaping head_a.
+		pf[0](head_a);
+	}
 	else if (ft_strcmp("sb",flags) == 0)
-		pf[1];
+	{
+		pf[0](head_b);
+	}
 	else if (ft_strcmp("ss",flags) == 0)
-		pf[2];
+	{
+		pf[0](head_a);
+		pf[0](head_b);
+	}
 	else if (ft_strcmp("pa",flags) == 0)
-		pf[3];
+	{
+		//push from tp of b to top of a.
+		pf[1](head_a, head_b);
+	}
 	else if (ft_strcmp("pb",flags) == 0)
-		pf[4];
+	{
+		pf[1](head_b, head_a);
+	}
 	else if (ft_strcmp("ra",flags) == 0)
-		pf[5];
+	{
+		pf[2](head_a);
+	}
 	else if (ft_strcmp("rb",flags) == 0)
-		pf[6];
+	{
+		pf[2](head_b);
+	}
 	else if (ft_strcmp("rr",flags) == 0)
-		pf[7];
+	{
+		pf[2](head_a);
+		pf[2](head_b);
+	}
 	else if (ft_strcmp("rra",flags) == 0)
 		pf[8];
 	else if (ft_strcmp("rrb",flags) == 0)
@@ -177,12 +230,15 @@ void	checkFunk(char *flags)
 		pf[10];
 }
 
+
 int		main(int argc, char** argv)
 {
-	t_list *head;
+	t_list *head; //head of a
+	t_list *h_b; //head of b
 
 	head = NULL;
 	readArgs(argc, argv, &head);
+
 	printFullList(head);
 	return (0);
 }
